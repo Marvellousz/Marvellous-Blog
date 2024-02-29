f85bf5e92e5359c74678c325f1e989736b9e9af5
Pranav Murali <pranavmurali024@gmail.com>
2024-02-29 23:28:50 +0530

    prop

--- src/Homepage.js
+++ src/Homepage.js
@@ -1,8 +1,18 @@
+import { useState } from 'react';
+import BlogList from './BlogList';
+
 const Homepage = () => {
+
+    const [blogs, setBlogs] = useState([
+        { title: 'My new website', body: 'lorem ipsum...', author: 'pranav', id: 1 },
+        { title: 'Welcome party!', body: 'lorem ipsum...', author: 'sagar', id: 2 },
+        { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'john', id: 3 }
+      ])
+
     return ( 
-        <div className="homepage">
-            <h2>Homepage</h2>
-        </div>
+        <div className="home">
+         <BlogList blogs={blogs} title="All Blogs"/>
+      </div>
      );
 }
  
